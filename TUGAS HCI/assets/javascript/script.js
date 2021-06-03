c564d2b2ad08ff3ed15e5259ae4111b48709bbb3
farhan-hafizh <farhannovyanh@gmail.com>
2021-06-03 10:58:30 +0700

    done not responsive char.html
    belum responsive

--- TUGAS HCI/assets/javascript/script.js
+++ TUGAS HCI/assets/javascript/script.js
@@ -1,5 +1,22 @@
+
+
 var slideIndex = 0;
 // showSlides();
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("slides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
 function showSlides() {
   var i;
   var slides = document.getElementsByClassName("slides");
